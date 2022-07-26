59de1de36558bbe32291e44acf9fb19f77e0d8ba
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-07-27 08:45:20 +1000

    Create hashing.py

---
+++ hashing.py
@@ -0,0 +1,11 @@
+import hashlib
+
+stored_password = hashlib.md5("Jayme".encode())
+
+entered_password = input("Enter Password: ")
+hashed_entered_password = hashlib.md5(entered_password.encode())
+
+if hashed_entered_password.hexdigest() == stored_password.hexdigest():
+    print("Logged in")
+else:
+    print("Wrong password")
